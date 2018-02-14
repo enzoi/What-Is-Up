9b1667bb55ba2c5004ff1785fdf3ee45bbe7fd1a
Yeontae Kim <ytk007@gmail.com>
2018-02-13 23:42:39 -0800

    initial commit

---
+++ WhatIsUp/MapController.swift
@@ -0,0 +1,25 @@
+//
+//  MapController.swift
+//  WhatIsUp
+//
+//  Created by Yeontae Kim on 2/13/18.
+//  Copyright © 2018 YTK. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
